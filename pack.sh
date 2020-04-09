83ea4b89d99ad49f68caeef6e06a28e9d1f70d7e
Gabriel Poesia Reis e Silva <me@gpoesia.com>
2020-04-09 16:48:38 -0700

    Add directory for experiment results

--- pack.sh
+++ pack.sh
@@ -3,9 +3,10 @@ rm -f $ZIPFILE
 zip -r $ZIPFILE \
   *.py \
   interactions.json \
-  dataset/medium.json \
+  dataset/large.json \
   models/__init__.py \
   figures/README \
+  results/README \
   "models/UniformEncoder(0.70)_0.0005_pythonalphabet.model" \
   "models/UniformEncoder(0.70)_0.0005_pythondecoder.model" \
   "models/UniformEncoder(0.70)_0.0005_python.json"
