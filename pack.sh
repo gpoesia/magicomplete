b63e964e4ad0affbcf687be0b6b6dedd3d085ca5
Gabriel Poesia Reis e Silva <me@gpoesia.com>
2020-05-07 18:35:54 -0700

    pack.sh: add only *.py to zip file

--- pack.sh
+++ pack.sh
@@ -2,11 +2,6 @@ ZIPFILE=conadcomplete.zip
 rm -f $ZIPFILE
 zip -r $ZIPFILE \
   *.py \
-  interactions.json \
-  dataset/large.json \
   models/__init__.py \
   figures/README \
-  results/README \
-  "models/UniformEncoder(0.70)_0.0005_pythonalphabet.model" \
-  "models/UniformEncoder(0.70)_0.0005_pythondecoder.model" \
-  "models/UniformEncoder(0.70)_0.0005_python.json"
+  results/README
