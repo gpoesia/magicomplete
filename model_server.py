3126e8be71b50e5ebfd32ace66ca0d28070da055
Gabriel Poesia Reis e Silva <me@gpoesia.com>
2020-07-03 20:20:29 -0700

    Use beam size of 6 when interacting with user

--- model_server.py
+++ model_server.py
@@ -4,12 +4,12 @@ from abbreviator import *
 from flask import Flask, request
 
 print('Loading model...')
-# 9ce32: 98.96% accuracy (CLM: 64d88)
-# 2dcfe: 97.80% accuracy
-# c90f3: 98.56% accuracy
+# 9ce32: 98.96% accuracy (CLM: 64d88, maxcol=3)
+# 2dcfe: 97.80% accuracy (CLM: bfe50, maxcol=inf)
+# c90f3: 98.56% accuracy (CLM: 3a64e, maxcol=3)
 # dce22: 97.28% accuracy
 # 5fa60: 96.09% accuracy
-abbreviator = CLMLanguageAbbreviator.load('models/64d88.model', device=torch.device('cpu'))
+abbreviator = CLMLanguageAbbreviator.load('models/2dcfe.model', device=torch.device('cpu'))
 
 app = Flask(__name__)
 
@@ -26,5 +26,5 @@ def complete():
     l = request.args.get('l', '')
     p = json.loads(request.args.get('p', '[]'))
     input = {'l': l, 'p': p, 'i': [], 'c': []}
-    completions = abbreviator.beam_search([input], 2)[0]
+    completions = abbreviator.beam_search([input], 8)[0]
     return json.dumps(completions[0])
